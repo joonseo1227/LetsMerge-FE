eacd3e621d4233daebca328fa59f28fec3343a4b
yongjun0702 <joks0702@gachon.ac.kr>
2025-01-11 21:53:55 +0900

    feat: map_tab.dart 파일 추가

--- lib/screens/main/map_tab.dart
+++ lib/screens/main/map_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 class MapTab extends StatelessWidget {
   const MapTab({super.key});
@@ -6,8 +7,20 @@ class MapTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: const Text('map'),
+      appBar: AppBar(
+        title: const Text('Map'),
+      ),
+      body: NaverMap(
+        options:  NaverMapViewOptions(
+          initialCameraPosition: NCameraPosition(
+            target: NLatLng(37.451039959670574, 127.12875330395157), // 초기 위치
+            zoom: 15.2, // 초기 줌 레벨
+          ),
+        ),
+        onMapReady: (controller) {
+          // 추가 코드
+          debugPrint('Naver Map is ready');
+        },
       ),
     );
   }
