cee85ebfc484cf98c735c8b4512d7829c23e4754
Minchan <kmc7582s@gmail.com>
2025-03-05 22:43:43 +0900

    chore: 택시 그룹 생성 코드 수정 및 예외처리

--- lib/provider/group_provider.dart
+++ lib/provider/group_provider.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:letsmerge/models/taxi_group/taxi_group.dart';
@@ -8,16 +11,33 @@ class TaxiGroupNotifier extends StateNotifier<List<TaxiGroup>> {
 
   final SupabaseClient _supabase = Supabase.instance.client;
 
-  Future<void> fetchTaxiGroups() async {}
+  Future<void> fetchTaxiGroups() async {
+    final User? user = _supabase.auth.currentUser;
+
+    if (user != null) {
+      final response = await _supabase.from('taxigroups')
+          .select()
+          .eq('host_id', user.id);
+
+      debugPrint('데이터 가져오기 성공: $response');
+    }
+  }
 
   Future<void> createTaxiGroup(TaxiGroup taxiGroup) async {
     final Map<String, dynamic> data = taxiGroup.toJson();
 
-    final response = await _supabase.from('taxigroups').insert(data);
-    if (response.error != null) {
-      debugPrint('택시 그룹 삽입 실패: ${response.error!.message}');
-    } else {
-      debugPrint('택시 그룹 삽입 성공: $response');
+    try {
+      await _supabase.from('taxigroups').insert(data);
+
+      debugPrint('택시 그룹 생성 성공');
+    } on FormatException catch (e) {
+      debugPrint('데이터 형식 오류: $e');
+    } on SocketException catch (_) {
+      debugPrint('네트워크 연결을 확인해주세요.');
+    } on TimeoutException catch (_) {
+      debugPrint('요청 시간이 초과되었습니다. 다시 시도해주세요.');
+    }  catch(e) {
+      debugPrint('알 수 없는 이유로 그룹 생성 실패: $e');
     }
   }
 }
